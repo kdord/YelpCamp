add1c769ab29e5e97188bcb887e5b566c8b3efb1
kdord <katya.koliada49@gmail.com>
2019-09-16 19:22:07 +0300

    comments post request added

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    Campground    = require("./models/campground.js"),
+    seedDB 		  = require("./seeds.js"),
+    Comment       = require("./models/comment.js")
 
+seedDB()
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true })
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
 
-// Schema setup
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-	  
-
-})
-var Campground = mongoose.model("Campground", campgroundSchema)
 
 
 
@@ -34,7 +29,7 @@ app.get('/campgrounds', function(req, res){
 			console.log('Smth went wrong!')
 			console.log(err)
 		} else {
-				res.render("index", {campgrounds: allCampgrounds});
+				res.render("campgrounds/index", {campgrounds: allCampgrounds});
 			}
 	})
 	 
@@ -60,9 +55,9 @@ app.post("/campgrounds", function(req, res){
 
 })
 
-
+//NEW
 app.get("/campgrounds/new", function(req, res){
-	res.render("new")
+	res.render("campgrounds/new")
 })
 
 
@@ -70,18 +65,66 @@ app.get("/campgrounds/new", function(req, res){
 
 app.get("/campgrounds/:id", function(req, res){
 	//find the campground with proveded id
-	Campground.findById(req.params.id, function(err, foundCampground){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
 		if (err) {
 			console.log(err)
 		} else {
+			console.log(foundCampground)
 			// render shoq template 
-			res.render("show", {campground: foundCampground})
+			res.render("campgrounds/show", {campground: foundCampground})
 		}
 	})
 	
 	// render show template with that campground
 
 })
+
+
+//=============================================
+// COMMENTS ROUTES
+
+app.get("/campgrounds/:id/comments/new", function (req,res) {
+	
+	// find campground by ID
+	Campground.findById(req.params.id, function (err, campground) {
+		if (err) {
+			console.log(err)
+		} else {
+			res.render("comments/new", {campground: campground})
+		}
+	})
+	
+})
+
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+	// lookup campground using id
+	Campground.findById(req.params.id, function (err, campground) {
+		if (err) {
+			console.log(err)
+			redirect("/campgrounds")
+		} else {
+			
+
+			Comment.create(req.body.comment, function(err, comment){
+				if (err) {
+					console.log(err)
+				} else {
+					campground.comments.push(comment)
+					campground.save()
+					res.redirect("/campgrounds/"+campground._id)
+				}
+			})
+		}
+	})
+	// create new comment
+	// connect new comment to campground
+	// redirect to campground show page
+})
+
+//=============================================
+
+
 
 app.listen(3000, function () {
 	console.log('The YelpCamp Server has started')
---
+++ seeds.js
@@ -0,0 +1,63 @@
+var  mongoose = require("mongoose")
+var Campground = require("./models/campground.js")
+var Comment = require("./models/comment.js")
+
+var data = [
+	{
+		name: "Camp1", 
+		image: "https://media.istockphoto.com/photos/friends-hikers-sitting-beside-camp-and-tents-in-the-night-picture-id678554980",
+		description: "blah-blah-blah" 
+	},
+	{
+		name: "Camp2", 
+		image: "https://media.istockphoto.com/photos/friends-hikers-sitting-beside-camp-and-tents-in-the-night-picture-id678554980",
+		description: "blah-blah-blah" 
+	},
+	{
+		name: "Camp3", 
+		image: "https://media.istockphoto.com/photos/friends-hikers-sitting-beside-camp-and-tents-in-the-night-picture-id678554980",
+		description: "blah-blah-blah" 
+	}
+]
+
+function  seedDB() {
+	// remove all campgrounds
+		Campground.deleteMany({}, function (err) {
+		if (err) {
+			console.log(err)
+		}
+		console.log("removed campground")
+		// add a few campgrounds
+		data.forEach(function(seed){
+			Campground.create(seed, function (err, campground) {
+				if (err) {
+					console.log(err)
+				} else {
+					console.log('added a campground');
+					//create a comment
+					Comment.create(
+						{text: "BLAH-BLAH-BLAH",
+						 author: "Homer"
+						}, function (err, comment) {
+							if (err) {
+								console.log(err)
+							} else {
+								campground.comments.push(comment);
+								campground.save()
+								console.log('Created a comment')
+							}
+							
+						})
+				}
+			})
+		})
+	})
+	
+
+	
+	// add a few comments
+}
+
+
+module.exports = seedDB
+
